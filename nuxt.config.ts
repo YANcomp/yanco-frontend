5812ffaacb922d40ace848bbd687b7308c32b56a
dnaugolnyi <dnaugolnyi@yandex.ru>
2024-02-03 20:59:07 +0300

    ver 0.1.03.02.2059

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import {createResolver} from "@nuxt/kit";
 
 export default defineNuxtConfig({
-  devtools: { enabled: false },
+    devtools: {enabled: false},
+    telemetry: false,
 })
