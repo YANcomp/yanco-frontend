66c3b9fdd09decbcc07daccd76e97b02040e3bba
dnaugolnyi <dnaugolnyi@yandex.ru>
2024-03-08 12:21:12 +0300

    ver 0.1.08.03.1215

---
+++ components/vPages/vArticleList.vue
@@ -0,0 +1,36 @@
+<script lang="ts">
+const props = defineProps({
+  categoryName: {
+    type: String
+  },
+  sectionName: {
+    type: String
+  },
+  category: {
+    type: Object
+  },
+  categories: {
+    type: Array,
+    default: function () {
+      return []
+    }
+  }
+})
+
+const categoryIDs = ref([])
+const isFailedGettingArticles = ref(!1)
+const isLoadingCategories = ref(!0)
+const isLocalLoading = ref(!1)
+const isRequest = ref(!1)
+const limit = ref(10)
+const offset = ref(0)
+const totalCount = ref(0)
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
