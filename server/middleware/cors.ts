16dcdf096b83e29ee63fa81a77f1a79f04fe0c56
dnaugolnyiWork <shaman.9602@gmail.com>
2024-03-16 12:55:34 +0300

    ver 0.1.16.03.1255

--- server/middleware/cors.ts
+++ server/middleware/cors.ts
@@ -1,5 +1,3 @@
-import {defineEventHandler, setResponseHeaders} from 'h3'
-
 export default defineEventHandler((event) => {
     setResponseHeaders(event, {
         "Access-Control-Allow-Methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
@@ -8,4 +6,9 @@ export default defineEventHandler((event) => {
         "Access-Control-Allow-Headers": '*',
         "Access-Control-Expose-Headers": '*'
     })
+    if(event.method === 'OPTIONS'){
+        event.node.res.statusCode = 204
+        event.node.res.statusMessage = "No Content."
+        return 'OK'
+    }
 })
