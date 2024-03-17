b25d133f5d33e41475b1b9c28a729c40c03d029a
dnaugolnyi <dnaugolnyi@yandex.ru>
2024-03-17 03:29:17 +0300

    ver 0.2.17.03.0329

--- plugins/api.ts
+++ plugins/api.ts
@@ -32,7 +32,7 @@ export default defineNuxtPlugin((nuxtApp) => {
     const {csrf} = useCsrf()
 
     const opts: Parameters<typeof $fetch<unknown>> [1] = {
-        baseURL: "http://cosmetic.yanco.ru/api",
+        baseURL: "https://cosmetic.yanco.ru/api",
     }
 
     opts.headers = (opts.headers || {}) as Record<string, string>
